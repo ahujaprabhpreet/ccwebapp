561dbe3042bbccb2271308ebd4e0851dd913d45f
puneetneu <tanwar.p@husky.neu.edu>
2019-06-06 22:06:11 -0400

    get book end points

--- webapp/src/main/java/com/neu/cloud/webapp/book/BookController.java
+++ webapp/src/main/java/com/neu/cloud/webapp/book/BookController.java
@@ -4,11 +4,12 @@ import com.neu.cloud.webapp.response.CustomResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class BookController {
@@ -26,6 +27,26 @@ public class BookController {
         bookRepository.save(book);
 
         return  new ResponseEntity(book, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/book")
+    public ResponseEntity getAllBooks(){
+
+        List<Book> books = bookRepository.findAll();
+        return  new ResponseEntity(books, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/book/{uuid}")
+    public ResponseEntity<Book> getBook(@PathVariable UUID uuid){
+
+        Optional<Book> book=bookRepository.findById(uuid);
+
+        if(!book.isPresent())
+            return new ResponseEntity(new CustomResponse(new Date(),"id not found","" ),HttpStatus.NOT_FOUND);
+
+        return  ResponseEntity.ok(book.get());
 
     }
 }
