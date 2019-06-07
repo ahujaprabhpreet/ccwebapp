e51a5dae3088a7b033e6a2f2c8b2614a8a79e555
nikunjlad <nikunj.sporty@gmail.com>
2019-06-06 21:19:01 -0400

    reponse functions added

--- webapp/src/main/java/com/neu/cloud/webapp/response/CustomResponse.java
+++ webapp/src/main/java/com/neu/cloud/webapp/response/CustomResponse.java
@@ -1,4 +1,40 @@
 package com.neu.cloud.webapp.response;
 
+import java.util.Date;
+
 public class CustomResponse {
+
+    private Date timestamp;
+    private String message;
+    private String Details;
+
+    public CustomResponse(Date timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        Details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return Details;
+    }
+
+    public void setDetails(String details) {
+        Details = details;
+    }
 }
