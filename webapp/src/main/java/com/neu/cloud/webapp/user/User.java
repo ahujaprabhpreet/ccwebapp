b674e7bea08a00b7b929d2ef38c909ecc4e0c18a
ahujaprabhpreet <ahuja.pra@husky.neu.edu>
2019-06-06 20:42:19 -0400

    user package updated

--- webapp/src/main/java/com/neu/cloud/webapp/user/User.java
+++ webapp/src/main/java/com/neu/cloud/webapp/user/User.java
@@ -1,5 +1,58 @@
 package com.neu.cloud.webapp.user;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name="Users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name="Id")
+    private Integer id;
+
+    @Column(name="Username")
+    private String username;
+
+    @Column(name="Password")
+    private String password;
+
+    @Column(name="Role")
+    private String role="USER";
+
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- webapp/src/main/java/com/neu/cloud/webapp/user/UserRepository.java
+++ webapp/src/main/java/com/neu/cloud/webapp/user/UserRepository.java
@@ -1,4 +1,11 @@
 package com.neu.cloud.webapp.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findUsersByUsername (String username);
+
+    public User findUsersByUsernameAndPassword(String username, String Password);
+
 }
