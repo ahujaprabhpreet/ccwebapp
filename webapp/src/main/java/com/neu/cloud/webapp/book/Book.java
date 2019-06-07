3f7741614e2e0d524614b5882cbd7c4399f90607
nikunjlad <nikunj.sporty@gmail.com>
2019-06-06 20:59:32 -0400

    Book model and Book Resource created

--- webapp/src/main/java/com/neu/cloud/webapp/book/Book.java
+++ webapp/src/main/java/com/neu/cloud/webapp/book/Book.java
@@ -1,4 +1,71 @@
 package com.neu.cloud.webapp.book;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name="Books")
 public class Book {
+
+    @Id
+    @Column(name="UUID")
+    private UUID uuid=UUID.randomUUID();
+
+    @Column(name="Title")
+    private String title;
+
+    @Column(name="Author")
+    private String author;
+
+    @Column(name="ISBN")
+    private String isbn;
+
+    @Column(name="Quantity")
+    private Integer quantity;
+
+    public Book() {
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
