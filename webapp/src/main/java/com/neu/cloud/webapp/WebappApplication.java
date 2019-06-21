5d6f2b85582ce29fcc834c9fb6a26ea6f5a371bc
ahujaprabhpreet <ahuja.pra@husky.neu.edu>
2019-06-21 15:45:31 -0400

    sprin intiailizr added

--- webapp/src/main/java/com/neu/cloud/webapp/WebappApplication.java
+++ webapp/src/main/java/com/neu/cloud/webapp/WebappApplication.java
@@ -2,12 +2,14 @@ package com.neu.cloud.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication extends SpringBootServletInitializer {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -16,6 +18,11 @@ public class WebappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebappApplication.class);
     }
 
 }
