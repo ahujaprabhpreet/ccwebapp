eb762b81507c56467f71844a9a931c1f31d01716
Nikunj Lad <lad.n@husky.neu.edu>
2019-07-19 01:31:50 -0400

    restored application endpoints to original name

--- webapp/src/main/java/com/neu/cloud/webapp/filestorage/ImageController.java
+++ webapp/src/main/java/com/neu/cloud/webapp/filestorage/ImageController.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/bookpp/{idBook}/image")
+@RequestMapping("/book/{idBook}/image")
 public class ImageController {
 
     @Autowired
